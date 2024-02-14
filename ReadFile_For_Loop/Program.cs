62a4dcfe76b39d9a4d00bad893c7d50e4e4849ae
Luis Alberto <layalarosa@gmail.com>
2024-02-14 02:33:25 -0400

    Chages Code ReadFile_For_Loop.

--- ReadFile_For_Loop/Program.cs
+++ ReadFile_For_Loop/Program.cs
@@ -9,63 +9,11 @@ class Program
         // Considerar usar Path no por FileInfo como esta abajo
         //var path = Path.GetFullPath(@"C:\Users\layal\OneDrive\Escritorio\Newsletter.csv");
 
-        FileInfo pathFile = new FileInfo(@"C:\Users\layal\OneDrive\Escritorio\Newsletter.csv");
-
-        //Abrir archivo para lectura/escritura
-        FileStream fs = pathFile.Open(FileMode.OpenOrCreate, FileAccess.Read, FileShare.Read);
-
-        StreamReader sr = new StreamReader(fs);
-
-        //Utilice el método ReadToEnd para leer todo el contenido del archivo. 
-        string fileContent = sr.ReadToEnd();
-
-        var arr = fileContent.Split('\n');
-
-        // 2 Forma
-        for (int i = 0; i < arr.Length; i++)
-        {
-            string[] arr2 = arr[i].Split(',');
-
-            //var toSave = new NewsletterContact();
-
-            //toSave.Name = arr2[2];
-            //toSave.Email = arr2[3];
-            //toSave.PreferredLocale = arr2[4];
-            //toSave.CreatedAt = time;
-            //toSave.UpdatedAt = time;
-
-            //_context.NewsletterContact.Add(toSave);
-            //_context.SaveChanges();
-
-        }
-
-        // NO BORRAR POR EL MOMENTO 
-        // for (int i = 0; i < arr.Length; i++)
-        // {
-        // string[] arr2 = arr[i].Split(',');
-
-        //    var toSave = new NewsletterContact();
-
-        //toSave.Name = arr2[2];
-        //toSave.Email = arr2[3];
-        //toSave.PreferredLocale = arr2[4];
-        //toSave.CreatedAt = time;
-        //toSave.UpdatedAt = time;
-
-        //    _context.NewsletterContact.Add(toSave);
-        //    _context.SaveChanges();
-
-        //}
-
-        Console.WriteLine(fileContent);
-
-        //Cerrar el objeto StreamReader después de la operación.
-        sr.Close();
-        fs.Close();
+        // ESTE CODIGO FUNCIONA PARA INSERTAR DATOS DE UN EXCEL A SQL POR MEDIO DE ENTITYFRAMEWORK
 
         //try
         //{
-        //    FileInfo pathFile = new FileInfo(@"C:\Users\layal\OneDrive\Escritorio\Newsletter.csv");
+        //    FileInfo pathFile = new FileInfo(@"C:\Users\layal\OneDrive\Escritorio\Newsletter - Copy.csv");
 
         //    //Abrir archivo para lectura/escritura
         //    FileStream fs = pathFile.Open(FileMode.OpenOrCreate, FileAccess.Read, FileShare.Read);
@@ -77,35 +25,43 @@ class Program
 
         //    var arr = fileContent.Split('\n');
         //    var time = DateTime.Now;
-
-        //    // 2 Forma
+           
         //    for (int i = 0; i < arr.Length; i++)
         //    {
-        //        string[] arr2 = arr[i].Split(',');
+        //        try
+        //        {
+        //            // Esto sirve como un tipo de index despues que hace 374 interaciones regresa a cero y vuelve a comenzar
+        //            if (i == 374)
+        //            {
+        //                var tt = 0;
+        //            }
 
-        //        var toSave = new NewsletterContact();
+        //            string[] arr2 = arr[i].Split(',');
 
-        //        toSave.Name = arr2[2];
-        //        toSave.Email = arr2[3];
-        //        toSave.PreferredLocale = arr2[4];
-        //        toSave.CreatedAt = time;
-        //        toSave.UpdatedAt = time;
+        //            var toSave = new NewsletterContact();
 
-        //        _context.NewsletterContact.Add(toSave);
-        //        _context.SaveChanges();
+        //            toSave.Name = arr2[2];
+        //            toSave.Email = arr2[3];
+        //            toSave.PreferredLocale = arr2[4];
+        //            toSave.CreatedAt = time;
+        //            toSave.UpdatedAt = time;
 
+        //            _context.NewsletterContact.Add(toSave);
+        //            _context.SaveChanges();
+        //        }
+        //        catch (Exception ex)
+        //        {
+        //            continue;
+        //        }
         //    }
-
-        //    //Console.WriteLine(fileContent);
-
         //    //Cerrar el objeto StreamReader después de la operación.
         //    sr.Close();
         //    fs.Close();
 
-        //} 
+        //}
         //catch (Exception ex)
         //{
-        //  Console.WriteLine(ex.Message);
+        //    Console.WriteLine(ex.Message);
         //}
     }
 }
